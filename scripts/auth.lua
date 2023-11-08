368107f8e52f95b5f7679fe2db6b209fc0984300
Electro <78709853+electro6666@users.noreply.github.com>
2023-11-08 06:36:04 +0530

    Update auth.lua

--- scripts/auth.lua
+++ scripts/auth.lua
@@ -1,18 +1,27 @@
--- example script that demonstrates response handling and
--- retrieving an authentication token to set on all future
--- requests
+-- Improved script for handling authentication and using a token in requests
 
-token = nil
-path  = "/authenticate"
+local token = nil
+local initialPath = "/authenticate"
+local resourcePath = "/resource"
 
-request = function()
-   return wrk.format("GET", path)
+-- Function to set authentication token in headers
+local function setAuthToken()
+   if not token then
+      return wrk.format("GET", initialPath)
+   else
+      wrk.headers["X-Token"] = token
+      return wrk.format("GET", resourcePath)
+   end
 end
 
+-- Function to handle the response and extract the token
 response = function(status, headers, body)
    if not token and status == 200 then
       token = headers["X-Token"]
-      path  = "/resource"
-      wrk.headers["X-Token"] = token
    end
+end
+
+-- Define the request function
+request = function()
+   return setAuthToken()
 end
