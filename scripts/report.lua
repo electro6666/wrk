e9647c5717cba98ab01d187a0489e85a195580d5
Electro <78709853+electro6666@users.noreply.github.com>
2023-11-08 06:33:33 +0530

    Update report.lua

--- scripts/report.lua
+++ scripts/report.lua
@@ -1,10 +1,14 @@
--- example reporting script which demonstrates a custom
--- done() function that prints latency percentiles as CSV
+-- Improved reporting script with custom done() function for printing latency percentiles as CSV
 
+-- Function to print latency percentiles as CSV
 done = function(summary, latency, requests)
-   io.write("------------------------------\n")
-   for _, p in pairs({ 50, 90, 99, 99.999 }) do
-      n = latency:percentile(p)
-      io.write(string.format("%g%%,%d\n", p, n))
-   end
+    local percentiles = {50, 90, 99, 99.999}
+
+    io.write("Percentile,Latency (ms)\n")
+    io.write("------------------------------\n")
+
+    for _, p in pairs(percentiles) do
+        local latencyValue = latency:percentile(p)
+        io.write(string.format("%g%%,%d\n", p, latencyValue))
+    end
 end
