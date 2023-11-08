7bcc750599c387b858004401913b4b21230045b0
Electro <78709853+electro6666@users.noreply.github.com>
2023-11-08 06:32:18 +0530

    Update setup.lua

--- scripts/setup.lua
+++ scripts/setup.lua
@@ -1,38 +1,54 @@
--- example script that demonstrates use of setup() to pass
--- data to and from the threads
+-- Improved script demonstrating the use of setup() to pass data to and from threads
 
 local counter = 1
 local threads = {}
 
+-- Function to set up each thread and pass data
 function setup(thread)
    thread:set("id", counter)
+   thread:set("requests", 0)  -- Initialize the requests counter
+   thread:set("responses", 0) -- Initialize the responses counter
    table.insert(threads, thread)
    counter = counter + 1
 end
 
+-- Function to initialize the script
 function init(args)
-   requests  = 0
+   -- Variables to keep track of global requests and responses
+   requests = 0
    responses = 0
 
-   local msg = "thread %d created"
-   print(msg:format(id))
+   local msg = "Thread %d created"
+   for _, thread in ipairs(threads) do
+      local id = thread:get("id")
+      print(msg:format(id))
+   end
 end
 
+-- Function to handle the request
 function request()
+   local threadRequests = wrk.thread:get("requests")
+   threadRequests = threadRequests + 1
+   wrk.thread:set("requests", threadRequests)
    requests = requests + 1
    return wrk.request()
 end
 
+-- Function to handle the response
 function response(status, headers, body)
+   local threadResponses = wrk.thread:get("responses")
+   threadResponses = threadResponses + 1
+   wrk.thread:set("responses", threadResponses)
    responses = responses + 1
 end
 
+-- Function to display the summary for each thread
 function done(summary, latency, requests)
-   for index, thread in ipairs(threads) do
-      local id        = thread:get("id")
-      local requests  = thread:get("requests")
-      local responses = thread:get("responses")
-      local msg = "thread %d made %d requests and got %d responses"
-      print(msg:format(id, requests, responses))
+   for _, thread in ipairs(threads) do
+      local id = thread:get("id")
+      local threadRequests = thread:get("requests")
+      local threadResponses = thread:get("responses")
+      local msg = "Thread %d made %d requests and got %d responses"
+      print(msg:format(id, threadRequests, threadResponses))
    end
 end
