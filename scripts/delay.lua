30114044f7d92bfb599001459cb7bb3ae798d7b1
Electro <78709853+electro6666@users.noreply.github.com>
2023-11-08 06:34:33 +0530

    Update delay.lua

--- scripts/delay.lua
+++ scripts/delay.lua
@@ -1,6 +1,10 @@
--- example script that demonstrates adding a random
--- 10-50ms delay before each request
+-- Improved script for adding a random delay before each request
 
+-- Define the minimum and maximum delay in milliseconds
+local minDelay = 10
+local maxDelay = 50
+
+-- Function to generate a random delay within the defined range
 function delay()
-   return math.random(10, 50)
+   return math.random(minDelay, maxDelay)
 end
