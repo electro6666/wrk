1aff76a541ab591de0cbfddcd5628ac5e48b1ae9
Electro <78709853+electro6666@users.noreply.github.com>
2023-11-08 06:40:19 +0530

    Update wrk.lua

--- src/wrk.lua
+++ src/wrk.lua
@@ -1,3 +1,5 @@
+-- Improved wrk.lua script
+
 local wrk = {
    scheme  = "http",
    host    = "localhost",
@@ -9,66 +11,70 @@ local wrk = {
    thread  = nil,
 }
 
+-- Function to resolve host and set up addresses
 function wrk.resolve(host, service)
    local addrs = wrk.lookup(host, service)
+
+   -- Filter and remove addresses that fail to connect
    for i = #addrs, 1, -1 do
       if not wrk.connect(addrs[i]) then
          table.remove(addrs, i)
       end
    end
+
    wrk.addrs = addrs
 end
 
+-- Function to set up each thread
 function wrk.setup(thread)
    thread.addr = wrk.addrs[1]
+
    if type(setup) == "function" then
       setup(thread)
    end
 end
 
-function wrk.init(args)
-   if not wrk.headers["Host"] then
-      local host = wrk.host
-      local port = wrk.port
-
-      host = host:find(":") and ("[" .. host .. "]")  or host
-      host = port           and (host .. ":" .. port) or host
-
-      wrk.headers["Host"] = host
-   end
-
-   if type(init) == "function" then
-      init(args)
-   end
-
-   local req = wrk.format()
-   wrk.request = function()
-      return req
-   end
-end
-
-function wrk.format(method, path, headers, body)
-   local method  = method  or wrk.method
-   local path    = path    or wrk.path
+-- Function to format the request
+function wrk.formatRequest(method, path, headers, body)
+   local method  = method or wrk.method
+   local path    = path or wrk.path
    local headers = headers or wrk.headers
-   local body    = body    or wrk.body
+   local body    = body or wrk.body
    local s       = {}
 
+   -- Ensure the Host header is set
    if not headers["Host"] then
-      headers["Host"] = wrk.headers["Host"]
+      local host = wrk.host
+      local port = wrk.port
+      host = host:find(":") and ("[" .. host .. "]") or host
+      host = port and (host .. ":" .. port) or host
+      headers["Host"] = host
    end
 
    headers["Content-Length"] = body and string.len(body)
 
    s[1] = string.format("%s %s HTTP/1.1", method, path)
+
    for name, value in pairs(headers) do
-      s[#s+1] = string.format("%s: %s", name, value)
+      s[#s + 1] = string.format("%s: %s", name, value)
    end
 
-   s[#s+1] = ""
-   s[#s+1] = body or ""
+   s[#s + 1] = ""
+   s[#s + 1] = body or ""
 
    return table.concat(s, "\r\n")
+end
+
+-- Function to initialize the script
+function wrk.init(args)
+   if type(init) == "function" then
+      init(args)
+   end
+
+   local req = wrk.formatRequest()
+   wrk.request = function()
+      return req
+   end
 end
 
 return wrk
